b509778b084a88555293cfd687937e06e18b3e95
David Maurer <davemaurermail@gmail.com>
2016-10-19 23:47:15 -0600

    Remove unneeded code in application controller. Add logged_in? method to sessions helper.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-
-  def test_page
-    render html: 'hello world'
-  end
 end
