91e6f85b97ff1becdb0d23e63203ab2e1c6b70b3
David Maurer <davemaurermail@gmail.com>
2017-03-06 22:22:46 -0700

    Add authenticated? method to User so we can authenticate returning users by their cookies.

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,8 @@ class User < ApplicationRecord
     self.remember_token = self.new_token
     update_attribute(:remember_digest, self.digest(remember_token))
   end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
