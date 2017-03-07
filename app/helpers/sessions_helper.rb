c62a0f73371d7d2c54129b94e4b26a0cc10a0df4
David Maurer <davemaurermail@gmail.com>
2017-03-06 22:27:57 -0700

    Add remember method to sessions helper and call remember user when creating a session.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,15 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 end
